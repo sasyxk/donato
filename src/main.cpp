5298d5edd59a02a64358f55ed2aaeecffedad2e9
SasyXk <donatobruno10@gmail.com>
2025-03-22 19:05:08 +0100

    Adding new file extension .donato

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,33 @@
 #include "codegen.h"
 #include "llvm/IR/Verifier.h"
 #include <iostream>
+#include <fstream>
+#include <sstream>
 
 int main(int argc, char** argv) {
     // Check if an argument (the input to compile) has been passed
     if (argc < 2) {
-        std::cerr << "Usage: " << argv[0] << " \"<expression>\"\n";
-        std::cerr << "Example: " << argv[0] << " \"let x = 5 in x * 2\"\n";
+        std::cerr << "Usage: " << argv[0] << " <file.donato>\n";
         return 1;
     }
 
-    // Read input from the first argument
-    std::string code = argv[1];
+    // Make sure the file name ends with ".donato"
+    std::string filename = argv[1];
+    const std::string ext = ".donato";
+    if (filename.size() < ext.size() || filename.substr(filename.size() - ext.size()) != ext) {
+        std::cerr << "Error: file extension must be " << ext << "\n";
+        return 1;
+    }
+
+    std::ifstream file(argv[1]);
+    if (!file) {
+        std::cerr << "Error opening file: " << argv[1] << "\n";
+        return 1;
+    }
+
+    std::stringstream buffer;
+    buffer << file.rdbuf();
+    std::string code = buffer.str();
 	
     Tokenizer tokenizer(code);
     Parser parser(tokenizer);
